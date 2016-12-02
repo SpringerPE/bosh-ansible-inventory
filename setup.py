bcfb79d7df90580409aba601fca25931adf73cc5
Jose Riguera <jriguera@gmail.com>
2016-12-02 12:06:03 +0100

    fix comma in classifiers

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 5 - Production/Stable'
+        'Development Status :: 5 - Production/Stable',
         # Indicate who your project is intended for
         'Intended Audience :: System Administrators',
         'Topic :: System :: Systems Administration',
@@ -80,7 +80,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3'
     ],
 
